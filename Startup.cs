c9b1bd13009cc3569b2fa4a40024c3f5708296dd
1feanyi <ifeanyi.igbo@gmail.com>
2017-02-13 22:01:45 -0500

    Init
    Initial commit. Working.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(patchmill.Startup))]
+namespace patchmill
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
